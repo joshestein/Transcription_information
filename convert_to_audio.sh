19b74c3dc576065e4286009fcc522c925020cd32
Josh <josh.e.stein@gmail.com>
2019-05-26 21:14:25 +0200

    Remove pre-existing files that ended with [0-9].wav

--- convert_to_audio.sh
+++ convert_to_audio.sh
@@ -12,10 +12,12 @@ function split_into_pieces {
                 OUT_FILE=$FILE_NAME-%03d.wav
                 ffmpeg -loglevel fatal -i "$i" -f segment -segment_time 50 -c copy $OUT_FILE;
         done
-
-        find . -type f -name '*[^0-9].wav' -delete # delete original files
 }
 
 cd $AUDIO_DIR
 convert_to_wav
 split_into_pieces
+
+# delete original files
+find $AUDIO_DIR -type f -name '*[^0-9].wav' -delete
+find $AUDIO_DIR -type f -name '*_[0-9].wav' -delete
